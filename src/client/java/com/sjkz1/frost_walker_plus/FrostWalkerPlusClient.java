280443a88b8b5365e6e3edd42a89225d7d2f2b57
Taratorn Kuakorboon <66944895+SJKZ1-2565@users.noreply.github.com>
2025-05-28 16:07:15 +0700

    Initial commit

---
+++ src/client/java/com/sjkz1/frost_walker_plus/FrostWalkerPlusClient.java
@@ -0,0 +1,10 @@
+package com.sjkz1.frost_walker_plus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FrostWalkerPlusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/sjkz1/frost_walker_plus/FrostWalkerPlus.java
@@ -0,0 +1,18 @@
+package com.sjkz1.frost_walker_plus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FrostWalkerPlus implements ModInitializer {
+	public static final String MOD_ID = "frost-walker-plus";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
