135882e7a6fc74e68a620f1a0949ec2b8518d63f
SJKZ1-2565 <seefuture13@gmail.com>
2025-12-26 21:03:12 +0700

    chore: Update to 1.21.8 and use architechtury api

---
+++ common/src/main/java/com/sjkz1/frostwalkerplus/Frostwalkerplus.java
@@ -0,0 +1,9 @@
+package com.sjkz1.frostwalkerplus;
+
+public final class Frostwalkerplus {
+    public static final String MOD_ID = "frostwalkerplus";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/sjkz1/frostwalkerplus/neoforge/FrostwalkerplusNeoForge.java
@@ -0,0 +1,12 @@
+package com.sjkz1.frostwalkerplus.neoforge;
+
+import com.sjkz1.frostwalkerplus.Frostwalkerplus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Frostwalkerplus.MOD_ID)
+public final class FrostwalkerplusNeoForge {
+    public FrostwalkerplusNeoForge() {
+        // Run our common setup.
+        Frostwalkerplus.init();
+    }
+}
